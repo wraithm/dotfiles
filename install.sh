e4848224bce31d89ab20e58ed4e1bb5662ff2e8a
Matthew Wraith <matt@bitnomial.com>
2018-07-22 22:40:32 -0500

    Fixing up git and git/hg ignore

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-linux_pkgs=(conky  ghc  git  hg  irc  mutt  term  vim  xmonad  zsh)
-macos_pkgs=(macos ghc  git  hg  vim  zsh)
+linux_pkgs=(conky  ghc  git  hg  irc  mutt  term  vim  xmonad  zsh psql)
+macos_pkgs=(ghc  hg  vim  zsh psql)
 
 if [[ "$1" == "linux" ]]; then
     pkgs="${linux_pkgs[@]}"
